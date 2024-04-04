fed93005bc94527258a72c5162516b2831744e2b
Mai614 <maymohamed614@gmail.com>
2024-04-03 17:00:07 -0700

    task01

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import logo from './Holberton_Logo.svg';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

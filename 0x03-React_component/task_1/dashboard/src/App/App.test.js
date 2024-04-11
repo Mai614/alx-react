94b9bffcd10018fe090afe51b5f633a747e544ef
Mai614 <maymohamed614@gmail.com>
2024-04-11 10:40:55 +0000

    task2

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,32 +1,40 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
+import { shallow } from 'enzyme';
+import React from 'react';
+import App from './App';
 
-describe("<App />", () => {
-  it("App renders without any errors", () => {
+describe('<App />', () => {
+  it('renders without crashing', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toEqual(true);
   });
 
-  it('Verify if CourseList is displayed when isLoggedIn is false', () => {
+  it('contain Notifications component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(CourseList)).toHaveLength(0);
+    expect(wrapper.find('Notifications')).toHaveLength(1);
   });
 
-  it('Verify if CourseList is displayed when isLoggedIn is false', () => {
-    const wrapper = shallow(<App isLoggedIn={true} />);
-    expect(wrapper.find(CourseList)).toHaveLength(1);
-    expect(wrapper.find(Login)).toHaveLength(0);
+  it('contain Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Header')).toHaveLength(1);
   });
 
-  it('logOut', () => {
-    const logOut = jest.fn(() => undefined);
-    const wrapper = shallow(<App logOut={logOut} />);
-    const alert = jest.spyOn(global, 'alert');
-    expect(alert);
-    expect(logOut);
-    jest.restoreAllMocks();
+  it('contain Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Login')).toHaveLength(1);
+  });
+
+  it('contain Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Footer')).toHaveLength(1);
+  });
+
+  it('CourseList', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('CourseList')).toHaveLength(0);
+  });
+
+  it('isLoggedIn true', () => {
+    const wrapper = shallow(<App isLoggedIn />);
+    expect(wrapper.find('Login')).toHaveLength(0);
+    expect(wrapper.find('CourseList')).toHaveLength(1);
   });
 });
